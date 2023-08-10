f4c2d66b4fcf9be74120d211994d2c1edb837328
Jose <jantoniogv85@gmail.com>
2023-08-10 19:18:04 +0200

    fix error conf pin rst lora

--- include/lora_init.h
+++ include/lora_init.h
@@ -13,16 +13,16 @@
 #define SCK 5   // GPIO5  -- SX1278's SCK
 #define MISO 19 // GPIO19 -- SX1278's MISO
 #define MOSI 27 // GPIO27 -- SX1278's MOSI
-#define SS 18   // GPIO18 -- SX1278's CS
-#define RST 14  // GPIO14 -- SX1278's RESET
+#define CS 18   // GPIO18 -- SX1278's CS
+#define RST 23  // GPIO23 -- SX1278's RESET
 #define DI0 26  // GPIO26 -- SX1278's IRQ(Interrupt Request)
 
 #define BAND_LORA 433E6
 
 void init_lora()
 {
-    SPI.begin(SCK, MISO, MOSI, SS);
-    LoRa.setPins(SS, RST, DI0);
+    SPI.begin(SCK, MISO, MOSI, CS);
+    LoRa.setPins(CS, RST, DI0);
 
     if (!LoRa.begin(BAND_LORA))
     {
